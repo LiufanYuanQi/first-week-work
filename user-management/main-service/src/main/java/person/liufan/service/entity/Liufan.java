4adc58df4fdbc831d354972684062433f1e125ec
LiufanYuanQi <875150049@qq.com>
2021-01-16 14:55:11 +0800

    master

--- user-management/main-service/src/main/java/person/liufan/service/entity/Liufan.java
+++
@@ -1,9 +0,0 @@
-package person.liufan.service.entity;
-
-/**
- * @author liufan E-mail:fan.liu@biz-united.com.cn
- * @version 创建时间：2021/1/16
- */
-public class Liufan {
-    String master = "master";
-}
